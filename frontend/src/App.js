835318a34a61d08fbdf5120dab1acb0c9ae99edc
Maximo Perez <maximo@wyeworks.com>
2024-08-18 14:06:16 -0300

    Initial files to initialize react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { QueryClientProvider } from "@tanstack/react-query";
+import {queryClient} from "./utils/queryClient"; // Asegúrate de que queryClient esté correctamente exportado
+import AppRouter from "./app/AppRouter";
+import { Provider } from 'react-redux';
+import store from './app/store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
+    </Provider>
+  );
+}
+
+export default App;
